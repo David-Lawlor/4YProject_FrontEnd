1ea65edf916c81147c221fb8358c41746eeaeb68
David <lawlor041988@gmail.com>
2017-02-15 21:08:38 +0000

    commit before starting login setup

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main')
+var ctrlMain = require('../controllers/main');
+var ctrlLogin = require('../controllers/login')
 
-router.get('/', ctrlMain.index);
+router.get('/', ctrlLogin.index);
+
+
 
 module.exports = router;
