2affc13ceaeaf30a30bf14e57ed3de4775914d6f
David <lawlor041988@gmail.com>
2017-03-30 00:33:12 +0100

    testing ip from request

--- app_server/controllers/mainCtrl.js
+++ app_server/controllers/mainCtrl.js
@@ -1,5 +1,34 @@
+var http = require('http');
+
 module.exports.index = function(req, res, next) {
+
+    var ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress;
+    console.log(ip);
+
+    var options = {
+        host: 'freegeoip.net',
+        path: '/json/' + ip,
+        method: 'GET'
+    };
+
+    var req1 = http.request(options, function(res) {
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+        });
+    });
+
+// write data to request body
+
+    req1.write('data\n');
+    req1.write('data\n');
+    req1.end();
     res.render('index', {
-        username: req.user.username
+        username: req.user.username,
+        ipaddress: ip
     });
 };
