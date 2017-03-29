9b58c475e4594e81e384b1c8798ab21dfa82345c
David <lawlor041988@gmail.com>
2017-03-30 00:47:36 +0100

    testing ip from request

--- app_server/controllers/mainCtrl.js
+++ app_server/controllers/mainCtrl.js
@@ -12,21 +12,6 @@ module.exports.index = function(req, res, next) {
         path: '/json/' + ip,
         method: 'GET'
     };
-
-    var req1 = http.request(options, function(res) {
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('BODY: ' + chunk);
-        });
-    });
-
-// write data to request body
-
-    req1.write('data\n');
-    req1.write('data\n');
-    req1.end();
     res.render('index', {
         username: req.user.username,
         ipaddress: ip
